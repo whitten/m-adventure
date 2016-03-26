da4061eb8c7c16a739584548ea38b3e71840e1c2
David Whitten <whitten@netcom.com>
2016-03-26 00:57:01 -0400

    Changes to be committed: 	modified:   C1GADV.m 	modified:   C1GADV2.m 	modified:   C1GADV3.m 	modified:   C1GADV4.m 	modified:   C1GADV5.m 	modified:   C1GADV6.m 	modified:   C1GADV7.m 	modified:   C1GADV8.m 	modified:   C1GADV9.m 	modified:   C1GADVDC.m 	modified:   XINDX2.m 	modified:   XINDX3.m
    Commit the code changes
    
    DO/GOTO Routines: ADV* -> C1GADV*
    change glvn Global: ^ADV(  -> ^C1GADV(711000,
    change glvn Global: ^ADVG( -> ^C1GADV(711000.1,

--- C1GADVDC.m
+++ C1GADVDC.m
@@ -1,5 +1,5 @@
-ADVDOC	;ADVENTURE.  FLH ; 25 JAN 83  10:32 AM
-	;;1.0;C1GAMES;****;March 1, 2016
+C1GADVDC	;ADVENTURE.  FLH ; 25 JAN 83  10:32 AM
+	;;1.0;C1GAMES;****;March 1, 2016;
 	;ADVENTURE.  FLH ; 25 JAN 83  10:32 AM
 	; DJW/WV ;March 1, 2016
 	;
@@ -10,53 +10,54 @@ I	; Adventure is the original computer game that has inspired dozens of
 	; Kent Blackett and Bob Supnick of Digital Equipment Corp. rewrote
 	; it in Fortran IV-Plus and then in Fortran IV for the PDP-11.
 	; Fred Hiltz of InterSystems Corp. translated it into ANSI Standard
-	; MUMPS--this program.
+	; MUMPS--this program. Adapted to be VistA and VA FileMan compatible
+	; by David Whitten
 	;
-II	; Any number can play simultaneously:  "D ^ADV" to get started.  Each
+II	; Any number can play simultaneously:  "D ^C1GADV" to get started.  Each
 	; player gives a name to his/her game, which can be suspended with a
 	; SUSPEND command at any time and later resumed.  Saved games more than
 	; ten days old are deleted automatically.
 	;
-III	; This program is written to run in InterSystems Corporation's M/11
-	; implementation of 1983 ANSI Standard MUMPS.  There are ten routines:
-	; AD, ADDOC, and AD2-AD9.  They require 40K bytes of disk storage.
-	; One global, ^ADV, contains the geography, text, etc.--it requires
-	; 84K bytes.  The second global, ^ADVG, holds the variables associated
-	; with each game in progress or suspended.  It requires 6K bytes per
-	; game.  Adventure should run in a partition of at least 6K bytes.
+III	; This program was originally written to run in InterSystems Corporation's
+	; M/11 implementation of 1983 ANSI Standard MUMPS. 
+	; There are ten routines: C1GADV, C1GADVDC, and C1GADV2-C1GADV9. 
+	; One global root, ^C1GADV(71100, contains the geography, text, etc.
+	; The second global root, ^C1GADV(71100.1, holds the variables
+	; associated with each game in progress or suspended.  
 	;
-IV	; Adventure is supplied on a magnetic tape containing a file of
-	; routines followed by a file of the global ^ADV.  Both files are
-	; written with 1024-byte physical records in the DOS format.  To
-	; install the game in M/11, mount the tape, sign in to the UCI in
-	; which the game will run, and D ^%RI.  Give tape parameters:
-	;     ("D3":0:1024)  if the tape is marked 800 bpi, or
-	;     ("D4":0:1024)  if the tape is marked 1600 bpi.
-	; Tell %RI to load all the routines.  The tape does not contain
-	; comments for ^COMMENT.  When %RI has finished, leave the tape in
-	; position and D ^%GI.  Give the same tape parameters.
+IV	; Adventure wzs originally supplied on a magnetic tape containing 
+	; a file of routines followed by a file of the global ^C1GADV. 
+	; Now, you can use the program D ^%RI. to load all the routines.  
+	; from the file adventure.rou When %RI has finished, you can use can
+	; load the globals with D ^%GI. with filename adventure.glo
+	; eventually it will be two KIDS files to transport both
 	;
-V	; After ^ADV is in place, you may optionally D ^%PROTECT and follow
-	; its instructions to allow read-only access to ^ADV from all UCI's,
+V	; After ^C1GADV is in place, you may optionally use a utility like
+	; D ^%PROTECT or whatever tool your vendor supplies and follow
+	; its instructions to allow read-only access to ^C1GADV from all UCI's,
 	; thus protecting it from change or deletion.  This completes the
 	; installation of Adventure.
 	;
 VI	; Adventure may easily be ported to other systems that support 1983
 	; ANSI Standard MUMPS.  The only non-standard features are confined
-	; to the routine ADV:  $ZS is the size of the partition in kilobytes.
+	; to the routine C1GADV:  $ZS is the size of the partition in kilobytes.
 	; $ZE governs trapping of errors and keyboard breaks.  Its code may be
-	; removed without detriment to the game.  The utility routine %ET is
+	; removed without detriment to the game.  The utility routine %ZTER is
 	; used to log the local variables following an error.
 	;
-VII	; For maintenance of Adventure please write, not telephone,
-	;          Frederick L. Hiltz, Ph. D.
-	;          Systems Development Engineer
-	;          InterSystems Corporation
-	;          210 Commercial Street, Third Floor
-	;          Boston, MA  02109
+VII	; For maintenance of Adventure please contact:
+	; 
+	;          David Whitten, MCS
+	;          Chief Technical Officer
+	;          WorldVistA
+	;          PO Box 269
+	;          Butner, NC 27509
+	;          Telephone:  713-870-3834
+	;          twitter:    davewhitten
+	;          repository: http://github.com/whitten/m-adventure/
 	;
 	; Maintenance will be provided only for the unaltered program, and only
-	; when a printout of the error logged by %ET is included (use the %ER
+	; when a printout of the error logged is included (use the ^XTER
 	; utility).  On a time-available basis, I will return either a patch
 	; or a new release of the program.
 	;
@@ -65,3 +66,7 @@ VIII	; some changes were made by DLH @ PRX in 1989
 	;
 IX	; changes by David Whitten @ WorldVistA in 2016 
 	; to comply with VA SACC & FileMan Compatibility
+	; 1st step was to remove any error XINDEX complained about
+	; 2nd step was to move everything to the namespace C1GADV
+	;            also move the global refs from ^ADV to ^C1GADV(71100,
+	;            and  move the global refs from ^ADVG to ^C1GADV(71100.1,
